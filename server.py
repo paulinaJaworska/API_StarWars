411edf7cf0a92d884a92c5665ed1194a45376c66
Paulina Jaworska <paulinajaworska9@gmail.com>
2019-05-02 00:04:20 +0200

    Created view for main page

--- server.py
+++ server.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template
 
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def route_index():
+    return render_template('planets.html')
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        port=5000
+    )
