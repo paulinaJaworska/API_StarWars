b4057f5e162054044463b9b4af4d57310827eb24
Paulina Jaworska <paulinajaworska9@gmail.com>
2019-05-02 01:30:04 +0200

    Created table structure and connection to database

---
+++ db_connection/sql_scripts/tables.sql
@@ -0,0 +1,29 @@
+ALTER TABLE IF EXISTS ONLY public.users DROP CONSTRAINT IF EXISTS pk_users_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.planet_votes DROP CONSTRAINT IF EXISTS pk_planet_votes_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.planet_votes DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
+
+
+DROP TABLE IF EXISTS public.users;
+CREATE TABLE users (
+                       id serial NOT NULL,
+                       username text NOT NULL UNIQUE ,
+                       password text NOT NULL
+);
+
+DROP TABLE IF EXISTS public.planet_votes;
+CREATE TABLE planet_votes (
+                              id serial NOT NULL,
+                              planet_id integer,
+                              planet_name text,
+                              user_id integer,
+                              submission_time timestamp without time zone
+);
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT pk_users_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY planet_votes
+    ADD CONSTRAINT pk_planet_votes_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY planet_votes
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
